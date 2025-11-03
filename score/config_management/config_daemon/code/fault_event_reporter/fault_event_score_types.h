6544fcb0f7c8a39bb662582505627f5d581a5803
Anton Krivoborodov <Anton.Krivoborodov@bmw.de>
2025-11-03 11:56:40 +0100

    initial commit

---
+++ score/config_management/config_daemon/code/fault_event_reporter/fault_event_score_types.h
@@ -0,0 +1,38 @@
+// *******************************************************************************
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
+
+#ifndef SCORE_SYSFUNC_CONFIGDAEMON_CODE_FAULT_EVENT_REPORTER_FAULT_EVENT_SCORE_TYPES_H
+#define SCORE_SYSFUNC_CONFIGDAEMON_CODE_FAULT_EVENT_REPORTER_FAULT_EVENT_SCORE_TYPES_H
+
+#include <cstdint>
+
+namespace score
+{
+namespace config_management
+{
+namespace config_daemon
+{
+namespace fault_event_reporter
+{
+
+enum class FaultEventId : std::uint8_t
+{
+    kUnkownError,
+};
+
+}  // namespace fault_event_reporter
+}  // namespace config_daemon
+}  // namespace config_management
+}  // namespace score
+
+#endif  // SCORE_SYSFUNC_CONFIGDAEMON_CODE_FAULT_EVENT_REPORTER_FAULT_EVENT_SCORE_TYPES_H
